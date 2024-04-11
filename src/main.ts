f5ce7883ea9d2b85c84579ad01763044ceee95b9
rosaTome <tomerosalinda@gmail.com>
2024-04-11 15:26:57 +0100

    scss styling background container

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
+// imports
 import "./style.scss";
+
+// querySelectors
+
+// null checks
+
+// functions 
+
+// functions - calling API
+    // make the response
+    // access the data 
+    // add the quote to the html
+
+// variables?
+
+// event listeners 
